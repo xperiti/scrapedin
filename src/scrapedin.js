325151eba84f91ddba49fd08d4c8c1bcd1272b10
Sebastian Pederiva <spederiva@gmail.com>
2020-02-25 11:32:25 +0200

    Added export.getToken

--- src/scrapedin.js
+++ src/scrapedin.js
@@ -27,11 +27,7 @@ module.exports = async ({ cookies, email, password, isHeadless, hasToLog, hasToG
     logger.warn('scrapedin', 'email/password and cookies wasn\'t provided, only public data will be collected')
   }
 
-  const token = await getToken(browser)
-  await browser.close()
-  return token
-
-  // return (url, waitMs) => profile(browser, cookies, url, waitMs, hasToGetContactInfo)
+  return (url, waitMs) => profile(browser, cookies, url, waitMs, hasToGetContactInfo)
 }
 
 const getToken = async (browser) => {
@@ -47,4 +43,33 @@ const getToken = async (browser) => {
   const token = linkedinCookies.find(c => c.name === 'li_at')
 
   return token && token.value
+}
+
+module.exports.getToken = async ({ cookies, email, password, isHeadless, hasToLog, hasToGetContactInfo, puppeteerArgs } = { isHeadless: true, hasToLog: false }) => {
+  if (!hasToLog) {
+    logger.stopLogging()
+  }
+  logger.info('scrapedin', 'initializing')
+
+  const args = Object.assign({ headless: isHeadless, args: ['--no-sandbox'] }, puppeteerArgs)
+  const browser = await puppeteer.launch(args)
+
+  if (cookies) {
+    logger.info('scrapedin', 'using cookies, login will be bypassed')
+  } else if (email && password) {
+    logger.info('scrapedin', 'email and password was provided, we\'re going to login...')
+
+    try {
+      await login(browser, email, password, logger)
+    } catch (e) {
+      await browser.close()
+      throw e
+    }
+  } else {
+    logger.warn('scrapedin', 'email/password and cookies wasn\'t provided, only public data will be collected')
+  }
+
+  const token = await getToken(browser)
+  await browser.close()
+  return token
 }
